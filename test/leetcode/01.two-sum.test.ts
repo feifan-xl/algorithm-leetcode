cfe08059de7389cdcf56dbde0a3753276e0d0ab0
feifan <906395192@qq.com>
2021-12-16 14:52:15 +0800

    添加单元测试相关

--- test/leetcode/01.two-sum.test.ts
+++ test/leetcode/01.two-sum.test.ts
@@ -1,6 +1,14 @@
 
 import { twoSum } from '../../src/leetcode/01.two-sum'
 
-test('twoSum', () => {
-  expect(twoSum([2,3,4], 6)).toEqual([2,4]);
+describe('twoSum', () => {
+
+  test('[2,3,4] => 6', () => {
+    expect(twoSum([2,3,4], 6)).toEqual([2,4]);
+  })
+
+  test('[1,2,3,4], 11', () => {
+    expect(twoSum([1,2,3,4], 11)).toEqual([]);
+  })
 })
+  
